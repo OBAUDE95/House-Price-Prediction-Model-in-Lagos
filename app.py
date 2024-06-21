656d7bae75448e8100261ec8ddc489d22d80566d
Obaude Ayodeji Michael <90180915+OBAUDE95@users.noreply.github.com>
2024-06-22 00:25:51 +0100

    Update app.py

--- app.py
+++ app.py
@@ -149,4 +149,4 @@ def update_output(n_clicks, bedrooms, bathrooms, toilets, parking_space, title):
 
 # Run the app
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server(debug=False)
